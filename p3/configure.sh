67ad81434175ec9e302abcea7fadf09284527b03
morgan <uoozhang@gmail.com>
2015-10-25 23:50:08 -0500

    p3 first attempt

---
+++ p3/configure.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#export CLASSPATH=~cs536-1/public/tools/deps_src/java-cup-11b.jar:~cs536-1/public/tools/deps_src/java-cup-11b-runtime.jar:~cs536-1/public/tools/deps:.
+export CLASSPATH=/home/alpha/cs536_fall2015/java_cup_src/bin/java-cup-11.jar:/home/alpha/cs536_fall2015/java_cup_src/classes/:.
